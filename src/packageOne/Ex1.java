675180f8031fb6ff37b8ef09a514dc5b6a21eb15
JN <994002514@qq.com>
2020-04-23 15:39:32 +0800

    test

---
+++ src/packageOne/Ex1.java
@@ -0,0 +1,11 @@
+package packageOne;
+
+/**
+ * @Author: jn
+ * @Date: 2020/4/23 15:35
+ */
+public class Ex1 {
+    public static void main(String args[]){
+        System.out.println("Hi World");
+    }
+}
